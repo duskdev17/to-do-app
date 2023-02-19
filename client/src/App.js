da42e6b80e3eb7b39c44ef61becb4bc64ecd1305
Dusk <65917000+duskdev17@users.noreply.github.com>
2023-02-19 14:47:41 +0600

    API and styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Welcome, Anik</h1>
+            <h4>Your tasks</h4>
+
+            <div className="todos">
+                <div className="todo">
+                    <div className="checkbox"></div>
+
+                    <div className="text">Solve Codes</div>
+
+                    <div className="delete-todo">x</div>
+                </div>
+
+                <div className="todo is-complete">
+                    <div className="checkbox"></div>
+
+                    <div className="text">Get Drinks</div>
+
+                    <div className="delete-todo">x</div>
+                </div>
+            </div>
+		</div>
+	);
 }
 
 export default App;
