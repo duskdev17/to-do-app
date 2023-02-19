2f8198b39602d07eb4b83a07717cabbc3767191c
Dusk <65917000+duskdev17@users.noreply.github.com>
2023-02-20 02:26:01 +0600

    front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,67 @@
+import { useState, useEffect } from 'react';
+
+const API_BASE = 'http://localhost:3001';
+
 function App() {
+    const [todos, setTodos] = useState([]);
+    const [popupActive, setPopupActive] = useState(false);
+    const [newTodo, setNewTodo] = useState("");
+
+    useEffect(() => {
+            GetTodos();
+    }, []);
+
+    const GetTodos = () => {  
+        fetch(API_BASE + "/todos")
+            .then(res => res.json())
+            .then(data => setTodos(data))
+            .catch(err => console.log("Error: ", err));
+    }   
+
+    const completeTodo = async id=> {
+        const data = await fetch(API_BASE + "/todo/complete/" + id)
+            .then(res => res.json())   
+
+        setTodos(todos => todos.map(todo => {
+            if (todo._id === data.id) {
+                todo.complete = data.complete;
+            }
+
+            return todo;
+        }));
+    }
+
+    const deleteTodo = async id => {
+        const data  = await fetch(API_BASE + "/todo/delete/" + id, {
+            method: "DELETE"
+        }).then(res => res.json());
+
+        setTodos(todos => todos.filter(todo => todo._id !== data.id));
+    }
+
 	return (
 		<div className="App">
 			<h1>Welcome, Anik</h1>
             <h4>Your tasks</h4>
 
             <div className="todos">
-                <div className="todo">
+                {todos.map(todo => (
+
+                <div className={
+                    "todo " + (todo.complete ? "is-complete" : "")} 
+                    key={todo._id} onClick={() => completeTodo(todo._id)}>
+
                     <div className="checkbox"></div>
 
-                    <div className="text">Solve Codes</div>
+                    <div className="text">{ todo.text }</div>
 
-                    <div className="delete-todo">x</div>
+                    <div className="delete-todo" onClick={() => deleteTodo
+                    (todo._id)}>x</div>
                 </div>
-
-                <div className="todo is-complete">
-                    <div className="checkbox"></div>
-
-                    <div className="text">Get Drinks</div>
-
-                    <div className="delete-todo">x</div>
-                </div>
+                ))}
             </div>
+
+            <div className="addPopup" onClick={() => setPopupActive(true)}>+</div>
 		</div>
 	);
 }
